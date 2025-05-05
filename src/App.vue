e753564bb026d0f03c570e2ec277be9bc08e68f2
FernJutamanee <jutamanee.thongbor@gmail.com>
2025-05-05 11:03:15 +0700

    Add animations

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import HomeView from './views/HomeView.vue'
 </script>
 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="Jutamanee!" />
+      <!-- <HelloWorld msg="Jutamanee!" /> -->
+      <HomeView />
     </div>
   </header>
 
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+    <section
+        class="flex flex-col items-center justify-center h-screen text-center bg-white text-gray-800 transition-all duration-700">
+        <h1 class="text-5xl font-bold mb-6 animate-fade-in-down">Hi, I'm Fern 🌿</h1>
+        <p class="text-lg mb-4 animate-fade-in-up">Frontend Developer | Vue & TypeScript</p>
+        <a href="#projects" class="bg-blue-500 text-white px-6 py-2 rounded-xl hover:bg-blue-600 transition">
+            View My Work
+        </a>
+    </section>
+</template>
